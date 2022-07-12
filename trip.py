7a09380fe3a9c39f1325fcb3f4e720d4343d3faf
MateoCastro8544 <davidxdcastro@gmail.com>
2022-07-12 18:06:03 -0500

    Metodos y Objetos Creados

---
+++ trip.py
@@ -0,0 +1,10 @@
+
+class Trip :
+    id              = int 
+    user            = str
+    car             = str
+    route           = str
+    amount          = float
+    payment         = str
+    progress        = str
+    completed       = bool
