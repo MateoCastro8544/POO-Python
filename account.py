63dd506585b7c1f1bc90a0ac4204f9de2c62383e
MateoCastro8544 <davidxdcastro@gmail.com>
2022-07-12 20:09:26 -0500

    Metodos y Objetos Creados

--- account.py
+++ account.py
@@ -1,8 +1,15 @@
 
 
+from typing_extensions import Self
+
+
 class Account :
     id          = int 
     name        = str
     document    = int 
     mail        = str
     password    = str 
+    
+    def __init__(self, name, document):
+        self.name       = name
+        self.document   = document
